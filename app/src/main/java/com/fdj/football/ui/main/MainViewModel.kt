019484cd1ea2f1f1a40b5fb63c6da8337dee4eb3
Reuniware <investdatasystems@yahoo.com>
2022-11-23 16:01:50 +0100

    Initial commit

---
+++ app/src/main/java/com/fdj/football/ui/main/MainViewModel.kt
@@ -0,0 +1,16 @@
+package com.fdj.football.ui.main
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+//    private val repository: FootballRepository
+) : ViewModel() {
+
+    fun maFonction() {
+
+    }
+
+}
