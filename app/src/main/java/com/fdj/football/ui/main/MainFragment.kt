2c692c2b493a80a413318e68abf7653b64825445
Reuniware <investdatasystems@yahoo.com>
2022-11-24 18:23:52 +0100

    recyclerview

--- app/src/main/java/com/fdj/football/ui/main/MainFragment.kt
+++ app/src/main/java/com/fdj/football/ui/main/MainFragment.kt
@@ -1,12 +1,18 @@
 package com.fdj.football.ui.main
 
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
 import com.fdj.football.R
+import com.google.gson.annotations.SerializedName
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -26,6 +32,8 @@ class MainFragment : Fragment() {
         viewModel.maFonction()
     }
 
+    private val adapter = GalleryAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,3 +42,80 @@ class MainFragment : Fragment() {
     }
 
 }
+
+
+data class UnsplashUser(
+    @field:SerializedName("name") val name: String,
+    @field:SerializedName("username") val username: String
+) {
+    val attributionUrl: String
+        get() {
+            return "https://unsplash.com/$username?utm_source=sunflower&utm_medium=referral"
+        }
+}
+
+data class UnsplashPhotoUrls(
+    @field:SerializedName("small") val small: String
+)
+
+data class UnsplashPhoto(
+    @field:SerializedName("id") val id: String,
+    @field:SerializedName("urls") val urls: UnsplashPhotoUrls,
+    @field:SerializedName("user") val user: UnsplashUser
+)
+
+
+
+class GalleryAdapter : PagingDataAdapter<UnsplashPhoto, GalleryAdapter.GalleryViewHolder>(GalleryDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GalleryViewHolder {
+        return GalleryViewHolder(
+            ListItemPhotoBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: GalleryViewHolder, position: Int) {
+        val photo = getItem(position)
+        if (photo != null) {
+            holder.bind(photo)
+        }
+    }
+
+    class GalleryViewHolder(
+        private val binding:
+    ) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.setClickListener { view ->
+                binding.photo?.let { photo ->
+                    val uri = Uri.parse(photo.user.attributionUrl)
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    view.context.startActivity(intent)
+                }
+            }
+        }
+
+        fun bind(item: UnsplashPhoto) {
+            binding.apply {
+                photo = item
+                executePendingBindings()
+            }
+        }
+    }
+}
+
+private class GalleryDiffCallback : DiffUtil.ItemCallback<UnsplashPhoto>() {
+    override fun areItemsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: UnsplashPhoto, newItem: UnsplashPhoto): Boolean {
+        return oldItem == newItem
+    }
+}
+
+
+
