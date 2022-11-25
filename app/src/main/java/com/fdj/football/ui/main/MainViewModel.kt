49cbe47d07ccabd54ef8c7d9450847f44cba8ef7
Reuniware <investdatasystems@yahoo.com>
2022-11-25 17:06:05 +0100

    recyclerview

--- app/src/main/java/com/fdj/football/ui/main/MainViewModel.kt
+++ app/src/main/java/com/fdj/football/ui/main/MainViewModel.kt
@@ -6,25 +6,47 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     var repository: FootballRepository,
-    var listOfLeagues: ArrayList<String> = ArrayList<String>()
 ) : ViewModel() {
     fun getAllLeagues() {
-        listOfLeagues = repository.getAllLeagues()
+        repository.getAllLeagues()
+    }
+}
+
+class FootballRepository @Inject constructor(private val service: FootballService) {
+    fun getAllLeagues() {
+
     }
 }
 
 @InstallIn(SingletonComponent::class)
 @Module
-object FootballRepository {
-    @Provides
-//    @Singleton
-    fun getAllLeagues(): ArrayList<String> {
-        return ArrayList<String>()
+interface FootballService {
+    companion object {
+        private const val BASE_URL = "https://api.unsplash.com/"
+
+        fun create(): FootballService {
+            val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(FootballService::class.java)
+        }
     }
 }
