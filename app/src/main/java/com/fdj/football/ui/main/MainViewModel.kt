6f529c98331e9dfd216dba95abc2d1349f851487
Reuniware <investdatasystems@yahoo.com>
2022-11-28 17:45:02 +0100

    recyclerview

--- app/src/main/java/com/fdj/football/ui/main/MainViewModel.kt
+++ app/src/main/java/com/fdj/football/ui/main/MainViewModel.kt
@@ -1,6 +1,9 @@
 package com.fdj.football.ui.main
 
 import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.annotations.SerializedName
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -36,24 +39,57 @@ interface FootballService {
     @GET("/")
     suspend fun getAllLeagues(
         @Query("query") query: String,
-    ):String
+        @Query("page") page: Int = 1,
+        @Query("per_page") perPage: Int = 5,
+        @Query("client_id") clientId: String = "null"
+    ):FootballSearchResponse
 
     companion object {
         private const val BASE_URL = "https://api.unsplash.com/"
 
         fun create(): FootballService {
-            val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+            val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
 
             val client = OkHttpClient.Builder()
                 .addInterceptor(logger)
                 .build()
 
+            val gson: Gson? = GsonBuilder().setLenient().create()
+
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .client(client)
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson!!))
                 .build()
                 .create(FootballService::class.java)
         }
     }
 }
+
+data class FootballSearchResponse(
+    @field:SerializedName("results") val results: List<UnsplashPhoto>,
+    @field:SerializedName("total_pages") val totalPages: Int
+)
+
+data class UnsplashPhoto(
+    @field:SerializedName("id") val id: String,
+    @field:SerializedName("urls") val urls: UnsplashPhotoUrls,
+    @field:SerializedName("user") val user: UnsplashUser
+)
+
+data class UnsplashPhotoUrls(
+    @field:SerializedName("small") val small: String
+)
+
+data class UnsplashUser(
+    @field:SerializedName("name") val name: String,
+    @field:SerializedName("username") val username: String
+) {
+    val attributionUrl: String
+        get() {
+            return "https://unsplash.com/$username?utm_source=sunflower&utm_medium=referral"
+        }
+}
+
+
+
